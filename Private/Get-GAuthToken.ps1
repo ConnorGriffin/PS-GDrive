8a0a6e87121063e5bc0e99b2f1bae257a7698ee5
Connor Griffin <connor.griffin@azdoa.gov>
2018-01-16 12:19:03 -0700

    Added Google Drive module framework

---
+++ Private/Get-GAuthToken.ps1
@@ -0,0 +1,29 @@
+function Get-GAuthToken {
+    <#
+    .SYNOPSIS
+        Return an access_token for the Google API
+    #>
+
+    # TODO: Make parameters mandatory
+
+    [CmdletBinding()]
+    Param(
+        [String]$RefreshToken,
+        [String]$ClientID,
+        [String]$ClientSecret
+    )
+
+    $params = @{
+        Uri = 'https://accounts.google.com/o/oauth2/token'
+        Body = @(
+            "refresh_token=$RefreshToken",
+            "client_id=$ClientID",
+            "client_secret=$ClientSecret",
+            "grant_type=refresh_token"
+        ) -join '&'
+        Method = 'Post'
+        ContentType = 'application/x-www-form-urlencoded'
+    }
+
+    Invoke-RestMethod @params
+}
