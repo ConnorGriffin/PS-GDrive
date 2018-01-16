15342c8cb6f42cb4fa2563067bc644ce5922cdf5
Connor Griffin <connor.griffin@azdoa.gov>
2018-01-16 14:04:10 -0700

    Cannot safely use PSDefaultParamters, instead return a header variable

---
+++ Private/Get-GAuthHeaders.ps1
@@ -0,0 +1,28 @@
+function Get-GAuthHeaders {
+    <#
+    .SYNOPSIS
+        Return headers for authenticating to Google's API
+    #>
+
+    # TODO: Make parameters mandatory
+
+    [CmdletBinding()]
+    Param(
+        [String]$RefreshToken,
+        [String]$ClientID,
+        [String]$ClientSecret
+    )
+
+    # Get an access token
+    $gAuthParam = @{
+        RefreshToken = $RefreshToken
+        ClientID = $ClientID
+        ClientSecret = $ClientSecret
+    }
+    $accessToken = Get-GAuthToken @gAuthParam
+
+    Return @{
+        "Authorization" = "Bearer $($accessToken.access_token)"
+        "Content-type" = "application/json"
+    }
+}
