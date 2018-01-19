83e36bbc28f185320b2182bf306662710be8e2f1
Connor Griffin <connor.griffin@azdoa.gov>
2018-01-19 11:24:11 -0700

    Build GDrive module framework

--- GDrive.psd1
+++ GDrive.psd1
@@ -69,7 +69,9 @@ Description = 'Google Drive API implementation.'
 # NestedModules = @()
 
 # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
-FunctionsToExport = '*'
+FunctionsToExport = @('Get-GAuthProfile',
+                      'Set-GAuthProfile',
+                      'New-GDriveItem')
 
 # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
 CmdletsToExport = '*'
--- GDrive.psm1
+++ GDrive.psm1
@@ -1,11 +1,29 @@
+# Install required modules
+$psd1 = Import-LocalizedData -BaseDirectory $PSScriptRoot -FileName (Get-ChildItem -Path $PSScriptRoot\*.psd1).Name
+$requiredModules = $psd1.PrivateData.PSData.ExternalModuleDependencies
+foreach ($module in $requiredModules) {
+    $installed = Get-Module -ListAvailable $module
+    if (!$installed) {
+        Install-Module $module -Scope CurrentUser -Force
+    }
+}
+
+# Get public and private function definition files.
+$functions  = Get-ChildItem -Path $PSScriptRoot\*\*.ps1 -ErrorAction SilentlyContinue
+
+# Dot source the files
+$functions.ForEach{
+    try {. $_.FullName}
+    catch {Write-Error -Message "Failed to import function $($_.FullName)"}
+}
+
+$moduleRoot = $PSScriptRoot
+
 # Create Profiles path if it does not exist
 $profilePath = "$ENV:APPDATA\PSModules\GDrive"
 if (!(Test-Path $profilePath)) {
     New-Item -Path $profilePath -Type Directory | Out-Null
 }
 
-$gAuthProfileExists = Test-Path "$profilePath\GDriveAuth.xml"
-if (!$gAuthProfileExists) {
-    Set-GAuthProfile
-}
-$gAuthParam = Get-GAuthProfile
+# Load/reload the GAuthProfile
+Initialize-GAuthProfile
