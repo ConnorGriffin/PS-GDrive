2b8a21f2e95f0a562ffc77376dd81e710360ea4a
Connor Griffin <connor.griffin@azdoa.gov>
2018-02-07 16:20:58 -0700

    Reverted GDrive PSDefaultParameterValues settings Caused conflicts with ParameterSets

--- Public/Get-GAuthProfile.ps1
+++ Public/Get-GAuthProfile.ps1
@@ -10,9 +10,14 @@ Function Get-GAuthProfile {
     $gAuth = Import-Clixml "$profilePath\GDriveAuth.xml"
 
     # Set default parameters for the rest of the script functions
-    $global:PSDefaultParameterValues['*:RefreshToken'] = $gAuth.RefreshToken
-    $global:PSDefaultParameterValues['*:ClientID'] = $gAuth.ClientID
-    $global:PSDefaultParameterValues['*:ClientSecret'] = $gAuth.ClientSecret
+    $global:PSDefaultParameterValues['*GDrive*:RefreshToken'] = $gAuth.RefreshToken
+    $global:PSDefaultParameterValues['*GAuth*:RefreshToken'] = $gAuth.RefreshToken
+
+    $global:PSDefaultParameterValues['*GDrive*:ClientID'] = $gAuth.ClientID
+    $global:PSDefaultParameterValues['*GAuth*:ClientID'] = $gAuth.ClientID
+
+    $global:PSDefaultParameterValues['*GDrive*:ClientSecret'] = $gAuth.ClientSecret
+    $global:PSDefaultParameterValues['*GAuth*:ClientSecret'] = $gAuth.ClientSecret
 
     if($gAuth.Proxy) {
         $global:PSDefaultParameterValues['*GDrive*:Proxy'] = $gAuth.Proxy
