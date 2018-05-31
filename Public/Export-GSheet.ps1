531ecd0735d09a925bd081d40e45708b4cfe65c9
Connor Griffin <connor.griffin@azdoa.gov>
2018-05-30 18:23:18 -0700

    Added mutli-tab support and pipeline support to Export-GSheet

--- Public/Export-GSheet.ps1
+++ Public/Export-GSheet.ps1
@@ -4,6 +4,22 @@ function Export-GSheet {
         Exports a PSObject to Google Sheets
     .DESCRIPTION
         Long description
+    .PARAMETER InputData
+        Object to be converted to spreadsheet data.
+    .PARAMETER SheetArray
+        Array of Spreadsheet objects to be created as multiple sheets in a single workbook.
+        Spreadsheet objects are created by passing the -ReturnSheet parameter with -InputData.
+    .PARAMETER SpreadsheetName 
+        Spreadsheet file name in Google Drive.
+    .PARAMETER SheetName
+        Sheet (tab) name.
+    .PARAMETER NoHeader
+        Do not output the header data to the sheet.
+    .PARAMETER ExcludeProperty
+        Exclude certain properties. Does not support wildcards.
+    .PARAMETER ReturnSheet 
+        Output the sheet object rather than uploading to Google Drive. 
+        Output from this command can be used in the $Sheet variable to create a multi-sheet spreadsheet.
     .EXAMPLE
         PS C:\> <example usage>
         Explanation of what the example does
@@ -14,33 +30,80 @@ function Export-GSheet {
     .NOTES
         General notes
     #>
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName='SingleSheet',
+                   PositionalBinding=$false)]
     param (
-        [PSCustomObject]$Data
+        [Parameter(ParameterSetName='SingleSheet',
+                   ValueFromPipeline=$true,
+                   Mandatory=$true)]
+        [Parameter(ParameterSetName='ReturnSheet',
+                   ValueFromPipeline=$true,
+                   Mandatory=$true)]
+        [PSObject]
+        $InputData,
+
+        [Parameter(ParameterSetName='MultiSheet',
+                   ValueFromPipeline=$true,
+                   Mandatory=$true)]
+        [PSObject[]]
+        $SheetArray,
+
+        [Parameter(Position=1,
+                   ParameterSetName='SingleSheet')]
+        [Parameter(Position=1,
+                   ParameterSetName='ReturnSheet')]
+        [String]
+        $SheetName = 'Sheet1',
+
+        [Parameter(Position=2,
+                   ParameterSetName='SingleSheet')]
+        [Parameter(Position=2,
+                   ParameterSetName='MultiSheet')]
+        [String]
+        $SpreadsheetName,
+
+        [Switch]
+        $NoHeader,
+
+        [String[]]
+        $ExcludeProperty,
+
+        [Parameter(ParameterSetName='ReturnSheet',
+                   Mandatory=$true)]
+        [Switch]
+        $ReturnSheet
     )
-    
+
     begin {
+        $valueData = @()
+        $sheetArray = @()
+        $firstRun = $true
     }
     
     process {
-        # Get the headers of the input data
-        $headerArray = $data[0].PSObject.Properties.Name
+        # Iterate through the actual data, adding to the array of values for each row
+        foreach ($row in $InputData) {
+            # On first iteration get the headers and add the header row (unless run with -NoHeader) 
+            if ($firstRun) {
+                $firstRun = $false
 
-        # Create data for the header rows
-        $valueData = @(
-            @{
-                values = $headerArray.ForEach{
-                    @{
-                        userEnteredValue = @{
-                            stringValue = $_
+                # Get the headers of the input data
+                $headerArray = $row.PSObject.Properties.Name | Where-Object {$_ -notin $ExcludeProperty}
+                
+                if (!$NoHeader) {
+                    # Add the row data for the header rows
+                    $valueData += @{
+                        values = $headerArray.ForEach{
+                            @{
+                                userEnteredValue = @{
+                                    stringValue = $_
+                                }
+                            }
                         }
                     }
                 }
             }
-        )
-
-        # Iterate through the actual data, adding to the array of values for each row
-        $valueData += foreach ($row in $data) {
+            
             # Get an array of each value in the row
             $values = $headerArray.ForEach{
                 
@@ -71,45 +134,63 @@ function Export-GSheet {
                 }
             }
 
-            # Return the array of values
-            @{
+            # Add the object to the valueData array
+            $valueData += @{
                 values = $values
             }
+        }
+    }
+    
+    end {
+        if ($PSCmdlet.ParameterSetName -in @('SingleSheet','ReturnSheet')) {
+            # Build the spreadsheet object if the parameter set is SingleSheet or ReturnSheet
+            # Format the spreadsheet object
+            $sheetObject = @{
+                properties = @{
+                    title = $SheetName
+                }
+                data = @{
+                    rowData = $valueData
+                }
+            }
+
+            # if -ReturnSheet, return the formatted sheet object only, no upload, etc. 
+            if ($ReturnSheet) {
+                return $sheetObject
+            }
+        } elseif ($PSCmdlet.ParameterSetName -in @('MultiSheet')) {
+            $sheetObject = $sheetArray
         }
 
         # Build the body object
         $body = @{
-            sheets = @(
-                @{
-                    properties = @{
-                        title = "BillingData"
-                    }
-                    data = @{
-                        rowData = $valueData
-                    }
-                }
-            )
-        } | ConvertTo-Json -Depth 20 -Compress
+            properties = @{}
+            sheets = @($sheetObject)
+        } 
+        
+        # Set the sheet title if specified
+        if ($SpreadsheetName) {
+            $body.properties.title = $SpreadsheetName
+        }
+        
+        $jsonBody = $body | ConvertTo-Json -Depth 20 -Compress
 
         # Set the upload headers
         $uploadHeaders = @{
             "Authorization" = $headers.Authorization
             "Content-Type" = 'application/json; charset=UTF-8'
-            "Content-Length" = $body.Length
+            "Content-Length" = $jsonBody.Length
         }
 
         # Splat the invoke-restmethod parameters for easy reading
         $restParams = @{
             Uri = 'https://sheets.googleapis.com/v4/spreadsheets/'
             Method = 'POST'
-            Body = $body
+            Body = $jsonBody
             Headers = $uploadHeaders 
             TimeoutSec = 60
         }
 
         Invoke-RestMethod @restParams
-    }
-    
-    end {
     }
 }
